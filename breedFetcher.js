32da2e478767df2b158f91a32adaace5416e414b
CliffGreene <lifeguardtwenty@gmail.com>
2020-05-08 12:32:45 -0600

    finalizing code refactor, working with error first callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,25 @@
-const request = require('request');
+const request = require("request");
+const breed = process.argv[2];
 
+// const fetchBreedDescription = function(breedName, callback) {
 
-const fetchBreedDescription = function(breedName, callback) {
+// };
 
-    request('https://api.thecatapi.com/v1/breeds/search?q=' + 
-   breedName, function(err, res, body) {
-  const data = JSON.parse(body);
-callback(err, data[0].description)
- 
-});
-  
-
-}
-
+const fetchBreedDescription = (breed, callback) => {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=` + breed,
+    (error, response, body) => {
+      if (error) {
+        callback("Error: verify details", null);
+      } else {
+        const data = JSON.parse(body);
+        if (data.length === 0) {
+          callback("Breed not found", null);
+        } else {
+          callback(null, data[0].description);
+        }
+      }
+    }
+  );
+};
+module.exports = { fetchBreedDescription }
